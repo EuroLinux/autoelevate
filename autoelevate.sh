0806a3d901da4f0a2488b34575de3873f08d7003
Kamil Aronowski <ka@euro-linux.com>
2022-06-24 11:49:50 +0200

    valid_distros

--- autoelevate.sh
+++ autoelevate.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/bin/bash -x
+
+set -euo pipefail
+
+distro=""
+valid_distros=( "almalinux" "centos" "eurolinux" "oraclelinux" "rocky" )
 
 usage() {
     echo "Usage: ${0##*/} [OPTIONS]"
@@ -6,13 +11,20 @@ usage() {
     echo "OPTIONS"
     echo "-d      Distribution to migrate to"
     echo ""
-    echo "Valid distributions are:"
-    echo "almalinux"
-    echo "centos"
-    echo "eurolinux"
-    echo "oraclelinux"
-    echo "rocky"
+    echo "Valid distributions are: ${valid_distros[@]}"
     exit 1
+}
+
+autoelevate() {
+  sudo yum install -y http://repo.almalinux.org/elevate/elevate-release-latest-el7.noarch.rpm
+  sudo yum install -y leapp-upgrade "leapp-data-${distro}"
+  sudo leapp preupgrade
+  sudo rmmod pata_acpi floppy mptbase mptscsih mptspi
+  echo PermitRootLogin no | sudo tee -a /etc/ssh/sshd_config
+  sudo sed -i 's@PermitRootLogin yes@PermitRootLogin no@g' /etc/ssh/sshd_config
+  sudo leapp answer --section remove_pam_pkcs11_module_check.confirm=True
+  sudo leapp upgrade
+  sudo reboot
 }
 
 while getopts "d:" option; do
@@ -21,13 +33,8 @@ while getopts "d:" option; do
         *) usage ;;
     esac
 done
-
-sudo yum install -y http://repo.almalinux.org/elevate/elevate-release-latest-el7.noarch.rpm
-sudo yum install -y leapp-upgrade "leapp-data-${distro}"
-sudo leapp preupgrade
-sudo rmmod pata_acpi floppy mptbase mptscsih mptspi
-echo PermitRootLogin no | sudo tee -a /etc/ssh/sshd_config
-sudo sed -i 's@PermitRootLogin yes@PermitRootLogin no@g' /etc/ssh/sshd_config
-sudo leapp answer --section remove_pam_pkcs11_module_check.confirm=True
-sudo leapp upgrade
-sudo reboot
+if [[ "${valid_distros[*]}" =~ "${distro}" ]]; then
+  autoelevate
+else
+  usage
+fi
