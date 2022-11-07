686f9834917b58a849d259ccf385a0884583fa3d
Kamil Aronowski <ka@euro-linux.com>
2022-11-07 15:56:26 +0100

    autoelevate.sh improvements

--- autoelevate.sh
+++ autoelevate.sh
@@ -14,15 +14,28 @@ usage() {
 }
 
 autoelevate() {
-  sudo yum install -y http://repo.almalinux.org/elevate/elevate-release-latest-el7.noarch.rpm
-  sudo yum install -y leapp-upgrade "leapp-data-${distro}"
-  sudo leapp preupgrade
-  sudo rmmod pata_acpi floppy mptbase mptscsih mptspi
-  echo PermitRootLogin no | sudo tee -a /etc/ssh/sshd_config
-  sudo sed -i 's@PermitRootLogin yes@PermitRootLogin no@g' /etc/ssh/sshd_config
-  sudo leapp answer --section remove_pam_pkcs11_module_check.confirm=True
-  sudo leapp upgrade
+  # Make sure CentOS Extras repo is available
+  curl "https://vault.centos.org/RPM-GPG-KEY-CentOS-7" > "/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7"
+  cat > "autoelevate-centos-extras.repo" <<-'EOF'
+[autoelevate-centos-7-extras]
+name=AutoELevate - CentOS 7 Extras
+mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+EOF
+
+  # First stage
+  sudo yum install -y http://repo.almalinux.org/elevate/elevate-release-latest-el7.noarch.rpm && \
+  sudo yum install -y leapp-upgrade "leapp-data-${distro}" && \
+  sudo leapp preupgrade || \
+  echo "Moving on with AutoELevate..." && \
+  sudo rmmod pata_acpi floppy mptbase mptscsih mptspi && \
+  echo PermitRootLogin no | sudo tee -a /etc/ssh/sshd_config && \
+  sudo sed -i 's@PermitRootLogin yes@PermitRootLogin no@g' /etc/ssh/sshd_config && \
+  sudo leapp answer --section remove_pam_pkcs11_module_check.confirm=True && \
+  sudo leapp upgrade && \
   sudo reboot
+  # Second stage will continue after reboot
 }
 
 while getopts "d:" option; do
@@ -31,7 +44,7 @@ while getopts "d:" option; do
         *) usage ;;
     esac
 done
-if [[ "${valid_distros[*]}" =~ "${distro}" ]]; then
+if [[ "${valid_distros[*]}" =~ "${distro}" ]] && [[ "$(rpm --eval %dist)" =~ ".el7" ]]; then
   autoelevate
 else
   usage
